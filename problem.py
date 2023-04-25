4910506da6f01ff2b8127665ef46025769e2c8f5
namithar99 <96168302+namithar99@users.noreply.github.com>
2023-04-25 11:37:56 -0500

    added problem2

---
+++ problem.py
@@ -0,0 +1,10 @@
+n = int(input("enter a number"))
+if n % 2!= 0:
+    print("Weird")
+else:
+   if 2 <= n <= 5:
+       print("Not Weird")
+   if 6 <= n <= 20:
+       print("Weird")
+   if n >= 20:
+       print("Not Weird")
---
+++ problem2.py
@@ -0,0 +1,4 @@
+n = int(input("enter a number"))
+for i in range(0,n):
+    print(i**2)
+    i = i+1
