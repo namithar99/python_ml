630954badca4e4a40dc567be59bceb1f8c3d33b4
namithar99 <96168302+namithar99@users.noreply.github.com>
2023-04-25 21:06:55 -0500

    Add leap_year

---
+++ leap_year.py
@@ -0,0 +1,23 @@
+year = int(input("enter a year:"))
+if(year%4 == 0 and year%100 == 0 and year%400 == 0):
+    print("yes a leap year")
+else:
+    print("not a leap year")
+
+
+year = int(input("enter a year"))
+if year%4 == 0:
+    if year%100 == 100:
+        if year%400 == 400:
+            print("yes a leap year")
+        else:
+            print("not a leap year")
+    else:
+        print("not a leap year")
+else:
+    print("not a leap year")
+
+
+
+
+
