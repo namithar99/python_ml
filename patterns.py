2d5277575c50538ba306e54032ee4b8230fcd2d2
namithar99 <96168302+namithar99@users.noreply.github.com>
2023-04-25 20:19:44 -0500

    Add patterns

---
+++ patterns.py
@@ -0,0 +1,21 @@
+for i in range(1,5):
+     for j in range(1,4):
+        print(1, end=" ")
+     print()
+
+
+ for i in range(1, 5):
+         for j in range(1, 4):
+          print(+, end=" "0
+        print()
+
+
+ for i in range(1,5):
+     for j in range(1,4):
+        print(1, end=" ")
+    print(i)
+
+for i in range(1, 5):
+        for j in range(1, 4):
+            print(1, end=" ")
+        print(j)
