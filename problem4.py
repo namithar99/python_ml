3cbe3053e67c896f9c8b0172c7a45e2f85b63b61
namithar99 <96168302+namithar99@users.noreply.github.com>
2023-04-26 07:48:37 -0500

    Add problem4

---
+++ problem4.py
@@ -0,0 +1,9 @@
+n = int(input("enter a number"))
+list1 = ["BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY", "BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY",
+        "BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY", "BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY"]
+list2 = [12, 30, 10, 5, 12, 30, 10, 5, 12, 30, 10, 5, 12, 30, 10, 5 ]
+print(n)
+for i in range(n):
+    print(list1[i], list2[i])
+
+
